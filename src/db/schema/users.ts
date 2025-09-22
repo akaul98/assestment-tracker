df592c340aa24240a66a287a011c5eab35fc6c09
Adarsh Kaul <adarsh.kaul@atidan.com>
2025-09-22 19:24:02 +0530

    feat: add global styles and layout for Next.js application
    - Implemented global CSS styles using Tailwind CSS and custom properties for theming.
    - Created a root layout component to wrap the application with font imports and metadata.
    - Developed the main page structure with responsive design and integrated images.
    - Set up database schema for users with Drizzle ORM and PostgreSQL.
    - Established database connection and utility functions for class name management.
    - Configured TypeScript settings for the project.

---
+++ src/db/schema/users.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { de } from "zod/locales";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  department: varchar({ length: 255 }).notNull(),
+  designation: varchar({ length: 255 }).notNull(),
+  status: varchar({ length: 50 }).notNull().default("active"),
+});
