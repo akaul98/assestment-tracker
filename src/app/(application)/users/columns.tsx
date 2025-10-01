b8ba535b779209f930f66573848ea19fc1bdcf34
Adarsh Kaul <ak.adarsh.kaul@gmail.com>
2025-10-01 12:46:46 +0530

    feat: add user data type and sample user data for custom data table

---
+++ src/app/(application)/users/columns.tsx
@@ -0,0 +1,28 @@
+import { ColumnDef } from "@tanstack/react-table"
+import {user} from "../../../components/custom-data-table"
+export const columns: ColumnDef<user, any>[] = [
+    {
+        accessorKey:"id",
+        header:"ID",
+    },
+    {
+        accessorKey:"name",
+        header:"Name",
+    },
+    {
+        accessorKey:"email",
+        header:"Email",
+    },
+    {
+        accessorKey:"designation",
+        header:"Designation",
+    },
+    {
+        accessorKey:"department",
+        header:"Department",
+    },
+    {
+        accessorKey:"status",
+        header:"Status",
+    },
+]
